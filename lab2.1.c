29ab29ee2969a05b95a1cf2a02118068505a8a55
Arthur Mironov <71429216+ArthurMironov@users.noreply.github.com>
2020-10-31 11:40:36 +0700

    Update lab2.1.c

--- lab2.1.c
+++ lab2.1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 long long get_fibbonachi_via_cycle(int number);
-int main()
-{
-  int number;
-  printf ("Enter n for find fibbonachi: ");
-  scanf ("%d", &number);
-  printf("%llu\t", get_fibbonachi_via_cycle(number + 1));
-}
+  int main()
+  {
+    int number;
+    printf ("Enter n for find fibbonachi: ");
+    scanf ("%d", &number);
+    printf("%llu\t", get_fibbonachi_via_cycle(number + 1));
+  }
 
 long long get_fibbonachi_via_cycle(int number)
 {
   long fib = 1;
   long fib_sum = 1;
-    for (int i = 0; i < number - 2; i++)
-    {
-      fib += fib_sum;
-      fib_sum = fib - fib_sum;
-    }
+  for (int i = 0; i < number - 2; i++)
+  {
+    fib += fib_sum;
+    fib_sum = fib - fib_sum;
+  }
 
   return fib_sum;
 }
