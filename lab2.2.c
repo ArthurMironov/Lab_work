4c3c03a243eebc882085b78083881697fc068d74
Arthur Mironov <71429216+ArthurMironov@users.noreply.github.com>
2020-10-31 13:17:08 +0700

    Update lab2.2.c

--- lab2.2.c
+++ lab2.2.c
@@ -3,6 +3,21 @@
 int cache[300];
 
 long long get_fibbonachi_via_recursion(int number)
+
+int main()
+{
+    int number;
+    printf("Enter n for find fibbonachi: ");
+    scanf("%d", &number);
+    for (int count = 1; count < number; count++)
+    {
+        printf("%llu\t\n", get_fibbonachi_via_recursion(count));
+    }
+      
+    return 0;
+}
+
+long long get_fibbonachi_via_recursion(int number)
 {
     if (cache[number] == 0)
     {
@@ -16,18 +31,5 @@ long long get_fibbonachi_via_recursion(int number)
         }
     }
 
-return cache[number];
-}
-
-int main()
-{
-    int number;
-    printf("Enter n for find fibbonachi: ");
-    scanf("%d", &number);
-    for (int count = 1; count < number; count++)
-    {
-        printf("%llu\t\n", get_fibbonachi_via_recursion(count));
-    }
-      
-return 0;
+    return cache[number];
 }
